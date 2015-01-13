580f21f3990a48f4f5640fadd8ccc9073c8bef8c
abrarmusa <abrar.musa.89@gmail.com>
2015-01-12 23:35:19 -0800

    added paperclip validator

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,6 +1,7 @@
 class Investor < ActiveRecord::Base
   has_one :user, as: :meta, dependent: :destroy
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage/
   accepts_nested_attributes_for :user
   has_many :shared_decks
 end
