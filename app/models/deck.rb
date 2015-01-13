282510c9dde7178ecb21984810fc12fd89207186
abrarmusa <abrar.musa.89@gmail.com>
2015-01-12 21:43:21 -0800

    cleaned up investor dashboard. Implemented viewing of shared decks

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,6 +1,6 @@
 class Deck < ActiveRecord::Base
   has_attached_file :pitch
-  validates_attachment :pitch, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
+  validates_attachment :pitch, :content_type => {:content_type => %w(image/jpeg image/jpg image/png application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/mspowerpoint application/vnd.ms-powerpoint application/vnd.openxmlformats-officedocument.presentationml.presentation)}
   has_attached_file :logo, :content_type => /\Aimage/, :default_url => "http://placehold.it/100x100"
   belongs_to :individual
 end
--- app/models/individual.rb
+++ app/models/individual.rb
@@ -1,6 +1,6 @@
 class Individual < ActiveRecord::Base
   has_one :user, as: :meta, dependent: :destroy
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/faceplace.png"
   accepts_nested_attributes_for :user
   has_many :decks
   has_many :shared_decks
