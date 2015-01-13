f72ba6207d5a5119a726a03decbd8a679c3ee73c
abrarmusa <abrar.musa.89@gmail.com>
2015-01-13 00:39:20 -0800

    fixed silly error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-    if current_user.meta_type = "Investor"
+    if current_user.meta_type == "Investor"
       investors_path(current_user)
     else
       individuals_path(current_user)
